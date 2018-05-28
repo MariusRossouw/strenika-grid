0f1ff76088c19e31c7dc3cb24b095ab5960fd517
mariusrossouw <marius@stratech.co.za>
2018-05-28 15:35:00 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'strenika-grid.js'
   },
   module: {
     rules: [
