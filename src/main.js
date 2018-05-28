be91e685a5d518f4b0d7630d026b33d51fc9a2bf
mariusrossouw <marius@stratech.co.za>
2018-05-28 16:07:55 +0200

    fixes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
-import StrenikaGrid from './App.vue';
+import StrenikaGrid from './StrenikaGrid.vue';
 
 import "../node_modules/ag-grid/dist/styles/ag-grid.css";
 import "../node_modules/ag-grid/dist/styles/ag-theme-balham.css";
 
-new Vue({
-  el: '#app',
-  render: h => h(StrenikaGrid)
-})
+if (document.querySelector('#strenika-grid-tester')){
+  Vue.component('strenika-grid', StrenikaGrid);
 
+  new Vue({
+      el: '#strenika-grid-tester'
+  });
+}
 
-export default StrenikaGrid
+export default StrenikaGrid;
